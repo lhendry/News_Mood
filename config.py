4187f24794f2f0a6c8beacfaf64416da4e2c8af3
lhendry <30734292+lhendry@users.noreply.github.com>
2017-12-02 21:31:57 -0600

    News_Mood Homework Upload

---
+++ config.py
@@ -0,0 +1,5 @@
+# Twitter API Keys
+consumer_key = "CzotZt11Pq5okW8tH7Dxqvo8k"
+consumer_secret = "LJ0lWYzK2HcaEqXH8AqEluBFUi8pbuMHyPBsvvZ38oiC4LH2iD"
+access_token = "930224510355296256-QuQ4PiOIGXZ1MaKm4jaIPzPpVHW2fSI"
+access_token_secret = "OKVcWABK5iLYG6NVwQumdAUHn0XSyk3LZaOe8uDhVT4c3"
